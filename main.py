b59248fc97ac3d50e3da3ce9d6d7c8fc859051ba
Yoon <74040890+memoday@users.noreply.github.com>
2022-12-26 16:16:49 +0900

    Update main.py
    최신버전 체크 알림 추가, QMenu 추가/수정

--- main.py
+++ main.py
@@ -6,8 +6,15 @@ import os, sys
 from requests_html import HTMLSession
 import re
 import webbrowser
+import requests
 
-__version__ = 'v1.0.1-alpha'
+__version__ = 'v1.0.1'
+
+latest_url = "https://api.github.com/repos/memoday/odiumWidget/releases/latest"
+gitAPI = requests.get(latest_url).json()
+print('Now version: '+__version__)
+print('Latest Version: '+gitAPI['tag_name'])
+__latest_version__ = gitAPI['tag_name']
 
 def resource_path(relative_path):
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
@@ -22,14 +29,17 @@ def updateValue():
     global value
 
     session = HTMLSession()
-    r = session.get('http://odium.kr')
-    r.html.render()
-    value = (r.html.find('#header > p',first=True)).text
-    session.close()
+    try: 
+        r = session.get('http://odium.kr')
+        r.html.render()
+        value = (r.html.find('#header > p',first=True)).text
+        session.close()
+    except:
+        value = 'Error'
     print('불러온 값: '+value)
 
 form_class = uic.loadUiType(form)[0]
-print('프로그램이 구동됩니다.')    
+print('프로그램이 구동됩니다.')   
 
 class SystemTrayIcon(QSystemTrayIcon):
 
@@ -41,10 +51,12 @@ class SystemTrayIcon(QSystemTrayIcon):
         urlAction = menu.addAction('사이트 바로가기')
         menu.addSeparator()
         infoAction = menu.addAction('프로그램 정보')
+        latestAction = menu.addAction('최신버전 다운')
         # creator = menu.addAction('제작자 | 오로라/창일')
         reloadAction.triggered.connect(lambda: updateValue())
         infoAction.triggered.connect(lambda: webbrowser.open_new_tab('https://github.com/memoday/odiumWidget'))
         urlAction.triggered.connect(lambda: webbrowser.open_new_tab('https://odium.kr'))
+        latestAction.triggered.connect(lambda: webbrowser.open_new_tab('https://github.com/memoday/odiumWidget/releases'))
         # creator.triggered.connect(lambda: webbrowser.open_new_tab('https://maple.gg/u/창일'))
 
         menu.addSeparator()
@@ -56,7 +68,7 @@ class SystemTrayIcon(QSystemTrayIcon):
 
     def Activation_Reason(self, index):
         if index == 2 :
-            print ("미구현")
+            webbrowser.open_new_tab('https://odium.kr')
 
 
 class Thread1(QThread):
@@ -86,7 +98,6 @@ class WindowClass(QWidget, form_class):
         shadow.setBlurRadius(15)
 
         self.offset = None
-
         self.settings = QSettings('memoday','odiumWidget')
 
         try: #위젯 마지막 위치 값 불러오기
@@ -94,7 +105,6 @@ class WindowClass(QWidget, form_class):
             self.move(int(posX),int(posY))
         except:
             self.move(1650, 20)
-
 
         self.setContextMenuPolicy(Qt.CustomContextMenu)
         self.customContextMenuRequested.connect(self.rightMenu)
@@ -119,11 +129,10 @@ class WindowClass(QWidget, form_class):
             bg_visible = (self.settings.value('bg_hidden'))
             if bg_visible == 'false': 
                 self.label_bg.show()
-                print('배경 표시 True')
             elif bg_visible == 'true':
                 self.label_bg.hide()
             else:
-                pass
+                self.label_bg.show()
         except:
             self.label_bg.show()
 
@@ -150,11 +159,6 @@ class WindowClass(QWidget, form_class):
 
         self.offset = None
         super().mouseReleaseEvent(event)
-
-        #버튼 기능
-    def main(self):
-        x = Thread1(self)
-        x.start()
 
     def rightMenu(self, pos):
         menu = QMenu()
@@ -192,10 +196,11 @@ class WindowClass(QWidget, form_class):
 
 if __name__ == "__main__":
     updateValue()
-    app = QApplication(sys.argv) 
+    app = QApplication(sys.argv)
     myWindow = WindowClass() 
     trayIcon = SystemTrayIcon(QIcon(icon))
     trayIcon.setToolTip('오디움 '+__version__)
     trayIcon.show()
-
+    if __version__ != __latest_version__:
+        trayIcon.showMessage("오디움 Odium","최신 버전이 발견되었습니다.\n웹사이트에서 다운받아 주시길 바랍니다.",QIcon(icon),10000)
     app.exec_()
